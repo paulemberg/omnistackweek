00f4e5ae5dd0973c10965b189d4284533bad11fa
Paulemberg Lima Silva <paulemberg.silva@digitalcode.com.br>
2019-10-02 18:49:57 -0300

    Ajustando spotController

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,7 +1,31 @@
+const User = require('../models/User')
+const Spot = require('../models/Spot')
+
 module.exports = {
     async store(req, res){
+
         console.log(req.body);
-        console.log(requ.file);
-        return res.json({ok: true})
+        console.log(req.file);
+        const {filename} = req.file;
+        const {company, techs, price} = req.body;
+        const {user_id} = req.headers;
+        
+        const user = User.findById(user_id);
+
+        if(!user){
+            return res.status(400).json({error: 'User does not exist'});
+        }
+
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        })
+        
+
+        return res.json({spot});
     }
 }
