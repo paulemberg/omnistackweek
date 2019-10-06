ce9e19153ca405c57343a00cb1931450f78d6451
Paulemberg Lima Silva <paulemberg.silva@digitalcode.com.br>
2019-10-06 18:54:16 -0300

    Finalizando 1‘ aula

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -2,6 +2,14 @@ const User = require('../models/User')
 const Spot = require('../models/Spot')
 
 module.exports = {
+    async index(req, res){
+        const {tech} = req.query;
+
+        const spots = await Spot.find({techs: tech});
+
+        return res.json(spots);
+    },
+
     async store(req, res){
 
         console.log(req.body);
